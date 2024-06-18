63cccfc0062a11381412f749b85dfe4ee1d9cbc5
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-18 09:26:18 +0900

    Initial Commit

---
+++ deepression/deepressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepressionApp.swift
+//  deepression
+//
+//  Created by 성대규 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct deepressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
