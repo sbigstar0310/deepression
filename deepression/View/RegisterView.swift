983ccb86060bfe268840a29358dd0adaf29203bd
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-20 15:25:19 +0900

    회원가입 뷰 이쁘게 다듬기

--- deepression/View/RegisterView.swift
+++ deepression/View/RegisterView.swift
@@ -29,106 +29,90 @@ struct RegisterView: View {
   
   var body: some View {
     ScrollView {
-      Text("이메일 주소 입력")
-        .fontWeight(.bold)
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      Text("이메일 주소를 적어주세요. (ex. abcdefg@gmail.com)")
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      TextField("이메일을 입력해주세요", text: $email)
-        .padding(10)
-        .textFieldStyle(.roundedBorder)
-        .border(Color.black)
-        .padding(.bottom, 5)
-        .padding(.horizontal, 31)
-      
-      HStack {
+      VStack(alignment: .leading) {
         Spacer()
-        Text("\(email.count)/22")
+          .frame(height: 50)
+        
+        Text("이메일 주소 입력")
+          .font(.title2)
+          .fontWeight(.bold)
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        Text("이메일 주소를 적어주세요. (예, aa@gmail.com)")
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        TextField("이메일을 입력해주세요", text: $email)
+          .padding(10)
+          .textFieldStyle(.roundedBorder)
+          .border(Color.black)
+          .padding(.bottom, 15)
+          .padding(.horizontal, 31)
+        
+        Text("비밀번호 설정")
+          .font(.title2)
+          .fontWeight(.bold)
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        Text("영문, 숫자 혼합하여 8자 이상으로 설정해주세요.")
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        SecureField("비밀번호", text: $password1)
+          .padding(10)
+          .textFieldStyle(.roundedBorder)
+          .border(Color.black)
+          .padding(.bottom, 15)
+          .padding(.horizontal, 31)
+        
+        Text("비밀번호 확인")
+          .font(.title2)
+          .fontWeight(.bold)
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        Text("위에 적어주신 비밀번호를 다시 한번 적어주세요")
+          .padding(.bottom, 12)
+          .padding(.horizontal, 31)
+        
+        SecureField("비밀번호 확인", text: $password2)
+          .padding(10)
+          .textFieldStyle(.roundedBorder)
+          .border(Color.black)
+          .padding(.bottom, 15)
+          .padding(.horizontal, 31)
       }
-      .padding(.bottom, 15)
-      .padding(.horizontal, 31)
-      
-      Text("비밀번호 설정")
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      Text("영문, 숫자 혼합하여 8자 이상으로 설정해주세요.")
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      SecureField("비밀번호", text: $password1)
-        .padding(10)
-        .textFieldStyle(.roundedBorder)
-        .border(Color.black)
-        .padding(.bottom, 5)
-        .padding(.horizontal, 31)
-      
-      HStack {
-        Spacer()
-        Text("\(password1.count)/22")
+    }
+    
+    if email.count > 0 && (password1 == password2) && (password1.count > 0) {
+      ZStack {
+        Rectangle()
+          .frame(width: 300, height: 60)
+          .foregroundStyle(.blue)
+        
+        Text("다음")
+          .foregroundStyle(.black)
       }
-      .padding(.bottom, 15)
-      .padding(.horizontal, 31)
-      
-      Text("비밀번호 확인")
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      Text("위에 적어주신 비밀번호를 다시 한번 적어주세요")
-        .padding(.bottom, 12)
-        .padding(.horizontal, 31)
-      
-      SecureField("비밀번호 확인", text: $password2)
-        .padding(10)
-        .textFieldStyle(.roundedBorder)
-        .border(Color.black)
-        .padding(.bottom, 5)
-        .padding(.horizontal, 31)
-      
-      HStack {
-        Spacer()
-        Text("\(password2.count)/22")
-      }
-      .padding(.bottom, 15)
-      .padding(.horizontal, 31)
-      .id("last")
-      
-      Spacer()
-        .frame(height: 50)
-      
-      
-      if email.count > 0 && (password1 == password2) && (password1.count > 0) {
-        ZStack {
-          Rectangle()
-            .frame(width: 300, height: 84)
-            .foregroundStyle(.blue)
-          
-          Text("다음")
-            .foregroundStyle(.black)
-        }
-        .onTapGesture {
-          Task {
-            await viewModel.doRegisterWith(email: email, password: password1)
-            guard let currentUserId = AuthManager.shared.getCurrentUserID() else {
-              return
-            }
-            userManager.user = User(id: currentUserId)
-            goRegisterView = false
+      .onTapGesture {
+        Task {
+          await viewModel.doRegisterWith(email: email, password: password1)
+          guard let currentUserId = AuthManager.shared.getCurrentUserID() else {
+            return
           }
+          userManager.user = User(id: currentUserId)
+          goRegisterView = false
         }
-      } else {
-        ZStack {
-          Rectangle()
-            .frame(width: 300, height: 84)
-            .foregroundStyle(.gray)
-          
-          Text("다음")
-            .foregroundStyle(.black)
-        }
+      }
+    } else {
+      ZStack {
+        Rectangle()
+          .frame(width: 300, height: 60)
+          .foregroundStyle(.gray)
+        
+        Text("다음")
+          .foregroundStyle(.black)
       }
     }
   }
