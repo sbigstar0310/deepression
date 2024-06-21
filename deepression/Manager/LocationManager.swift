713fb0b7a0ec37a0a1e57acba663b2b9581af948
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-21 15:53:14 +0900

    서버에 업데이트 하는 로직을 LocationUpdateManager로 이관

--- deepression/Manager/LocationManager.swift
+++ deepression/Manager/LocationManager.swift
@@ -10,12 +10,8 @@ import CoreLocation
 import UIKit
 
 class LocationManager: NSObject, ObservableObject {
-  @Published var lastUpdatedDate: Date = Date(timeIntervalSince1970: 0)
-  
   private var locationManager = CLLocationManager()
-  private let fbRealtimeDataManager = FBRealtimeDataManager.shared
-  private let userManager = UserManager.shared
-  private var timer: Timer?
+  private let locationUpdateManager = LocationUpdateManager()
   
   static let shared = LocationManager()
   override private init() {
@@ -31,28 +27,17 @@ class LocationManager: NSObject, ObservableObject {
     locationManager.pausesLocationUpdatesAutomatically = false
   }
   
-  
-  func startUpdateLocationWithTimer() {
-    // 주어진 시간(sec)마다 위치 정보 업데이트
-    let sec: Double = 15 * 60 // 15분 마다 서버에 업로드
-    timer = Timer.scheduledTimer(timeInterval: sec, target: self, selector: #selector(requestLocation), userInfo: nil, repeats: true)
-    RunLoop.current.add(timer!, forMode: .default)
-    
-    // 사용자의 장거리 위치 변화 감지
-    startSignificantChangeUpdates()
-  }
-  
-  @objc func requestLocation() {
-    // 사용자 현재 위치 요청
-    locationManager.requestLocation()
-  }
-  
-  func stopUpdateLocationWithTimer() {
-    timer?.invalidate()
-    timer = nil
-  }
-  
   func startUpdatingLocation() {
+    guard CLLocationManager.locationServicesEnabled() else {
+      print("CLLocationManagager: 위치 정보를 받아올 수 없음.")
+      return
+    }
+    
+    guard CLLocationManager.headingAvailable() else {
+      print("CLLocationManagager: 헤딩(방향) 정보를 사용할 수 없음.")
+      return
+    }
+    
     locationManager.startUpdatingLocation()
   }
   
@@ -83,50 +68,15 @@ class LocationManager: NSObject, ObservableObject {
 // MARK: - CLLocationMangerDelegate에 대한 코드
 extension LocationManager: CLLocationManagerDelegate {
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    let calendar = Calendar.current
-    let currentDate = Date()
-    
-    // 마지막 업데이트 날짜로부터 업데이트 간격 구하기
-    guard let differenceInMinutes = Calendar.current.dateComponents([.minute], from: lastUpdatedDate, to: currentDate).minute else {
-      print("오류: 시간 차이가 nil입니다.")
+    guard let cllocation = locations.last else {
+      print("Can't get location")
       return
     }
     
-    if differenceInMinutes < 15 {
-      // 시간 차이가 15분 이내인 경우: Update Pass
-      return
-    }
+    let location = Location(updatedDate: Date(), latitude: cllocation.coordinate.latitude, longitude: cllocation.coordinate.longitude)
     
-    // 앱의 상태 확인
-    let appState = UIApplication.shared.applicationState
     
-    switch appState {
-    case .active:
-      print("환경: Foreground")
-    case .background:
-      print("환경: Background")
-    default:
-      print("환경: 기타")
-    }
-    
-    print("업데이트 시간: \(fbDateFormatter.string(from: currentDate))")
-    
-    if let location = locations.last {
-      print("위도: \(location.coordinate.latitude), 경도: \(location.coordinate.longitude)")
-      
-      guard let user = userManager.user else {
-        print("서버 업로드 실패: 유저가 로그인 상태가 아닙니다.")
-        return
-      }
-      
-      // 마지막 업데이트 날짜 최신화
-      lastUpdatedDate = currentDate
-      
-      // 서버에 위치 정보 업로드
-      fbRealtimeDataManager.addUserData(user: user, latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-    }
-    
-    print("---------------------------------------------------------")
+    locationUpdateManager.UpdateLocationToFireBase(location: location)
   }
   
   func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
@@ -139,3 +89,7 @@ extension LocationManager: CLLocationManagerDelegate {
     print("권한이 변경되었습니다:  \(manager.authorizationStatus)")
   }
 }
+
+// MARK: - CoreLocation Error extension
+
+
