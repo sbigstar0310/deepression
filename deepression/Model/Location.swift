f89d157c1dbc908b863f99738f2e4e36e227a048
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-21 15:52:13 +0900

    사용자의 위치정보 구조체 Location 추가

---
+++ deepression/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  deepression
+//
+//  Created by 성대규 on 6/21/24.
+//
+
+import Foundation
+
+struct Location: Codable, Hashable {
+    let updatedDate: Date
+    let latitude: Double
+    let longitude: Double
+}
