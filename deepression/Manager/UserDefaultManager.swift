991e70443c8c0e8ee628d2f3c417545ad11b75eb
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-24 17:05:25 +0900

    indentation 정리

--- deepression/Manager/UserDefaultManager.swift
+++ deepression/Manager/UserDefaultManager.swift
@@ -10,38 +10,38 @@ import Foundation
 
 class UserDefaultManager {
   func setLastUpdateDate(date: Date) {
-      let defaults = UserDefaults.standard
-      defaults.set(date, forKey: "lastUpdateDate")
+    let defaults = UserDefaults.standard
+    defaults.set(date, forKey: "lastUpdateDate")
   }
   
   func getLastUpdateDate() -> Date? {
-      let defaults = UserDefaults.standard
-      return defaults.object(forKey: "lastUpdateDate") as? Date
+    let defaults = UserDefaults.standard
+    return defaults.object(forKey: "lastUpdateDate") as? Date
   }
   
   func setLocations(locations: [Location]) {
-      let defaults = UserDefaults.standard
-      let encoder = JSONEncoder()
-      do {
-          let data = try encoder.encode(locations)
-          defaults.set(data, forKey: "locations")
-      } catch {
-          print("Failed to encode locations: \(error)")
-      }
+    let defaults = UserDefaults.standard
+    let encoder = JSONEncoder()
+    do {
+      let data = try encoder.encode(locations)
+      defaults.set(data, forKey: "locations")
+    } catch {
+      print("Failed to encode locations: \(error)")
+    }
   }
   
   func getLocations() -> [Location]? {
-      let defaults = UserDefaults.standard
-      if let data = defaults.data(forKey: "locations") {
-          let decoder = JSONDecoder()
-          do {
-              let locations = try decoder.decode([Location].self, from: data)
-              return locations
-          } catch {
-              print("Failed to decode locations: \(error)")
-          }
+    let defaults = UserDefaults.standard
+    if let data = defaults.data(forKey: "locations") {
+      let decoder = JSONDecoder()
+      do {
+        let locations = try decoder.decode([Location].self, from: data)
+        return locations
+      } catch {
+        print("Failed to decode locations: \(error)")
       }
-      return nil
+    }
+    return nil
   }
   
   func addLocation(location: Location) {
