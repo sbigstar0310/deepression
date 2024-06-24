6adda938ebb76f271553e2774e3cf9af7a5bc3ae
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-24 17:05:01 +0900

    Wifi 구조체 추가

---
+++ deepression/Model/Wifi.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  deepression
+//
+//  Created by 성대규 on 6/24/24.
+//
+
+import Foundation
+
+struct Wifi {
+  let updatedDate: Date
+  let ssid: String
+  let bssid: String
+  let rssi: Double
+}
