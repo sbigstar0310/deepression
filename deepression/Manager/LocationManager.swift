44fd376dd3ce64be8bc2647796fb520b153ec1b4
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-25 14:41:05 +0900

    위치 설정 상태 변수 추가, lock을 통해 업데이트 로직 원자화, 권한 변경시 설정 권한 변수에 바로 반영하도록 적용

--- deepression/Manager/LocationManager.swift
+++ deepression/Manager/LocationManager.swift
@@ -10,6 +10,8 @@ import CoreLocation
 import UIKit
 
 class LocationManager: NSObject, ObservableObject {
+  @Published var authorizationStatus: CLAuthorizationStatus?
+  
   private let locationManager = CLLocationManager()
   private let networkManager = NetworkManager.shared
   private let fbUpdateManager = FBUpdateManager.shared
@@ -64,21 +66,34 @@ class LocationManager: NSObject, ObservableObject {
     // 항상 사용 권한 요청
     locationManager.requestAlwaysAuthorization()
   }
+  
+  private var lock = false
 }
 
 // MARK: - CLLocationMangerDelegate에 대한 코드
 extension LocationManager: CLLocationManagerDelegate {
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    // print("위치 업데이트 호출 됨 \(msDateFormatter.string(from: Date()))")
+    
     guard let cllocation = locations.last else {
       print("Can't get location")
       return
     }
     
     Task {
+      guard !lock else {
+        // 아직 업데이트 중
+        return
+      }
+      
+      lock = true
+      
       let wifi = await networkManager.getCurrentWiFiInfo()
       let location = Location(updatedDate: Date(), latitude: cllocation.coordinate.latitude, longitude: cllocation.coordinate.longitude)
       await fbUpdateManager.updateWifiToFirebase(wifi: wifi)
       await fbUpdateManager.updateLocationToFireBase(location: location)
+      
+      lock = false
     }
   }
   
@@ -90,6 +105,25 @@ extension LocationManager: CLLocationManagerDelegate {
   func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
     // 권한 변경시
     print("권한이 변경되었습니다:  \(manager.authorizationStatus)")
+    switch manager.authorizationStatus {
+    case .authorizedWhenInUse:
+      print("authorizedWhenInUse")
+      authorizationStatus = .authorizedWhenInUse
+    case .authorizedAlways:
+      print("authorizedAlways")
+      authorizationStatus = .authorizedAlways
+    case .notDetermined:
+      print("notDetermined")
+      authorizationStatus = .notDetermined
+    case .denied:
+      print("denied")
+      authorizationStatus = .denied
+    case .restricted:
+      print("restricted")
+      authorizationStatus = .restricted
+    default:
+      break
+    }
   }
 }
 
