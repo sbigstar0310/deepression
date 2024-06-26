78e3a0f1ea77466c1122b265039a0b5ed937bc7b
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-26 15:36:39 +0900

    Network Extension과 Network이 중복되는 문제: NetworkExtension을 사용하는 코드를 Wifi Manager로 구분함

--- deepression/Manager/WIfiManager.swift
+++ deepression/Manager/WIfiManager.swift
@@ -7,3 +7,27 @@
 
 import Foundation
 import NetworkExtension
+
+class WifiManager {
+  // 싱글톤 클래스
+  static let shared = WifiManager()
+  private init() {}
+  
+  // WIFI 연결 정보 가져오기
+  func getCurrentWiFiInfo() async -> Wifi {
+    let currentWifi = await NEHotspotNetwork.fetchCurrent()
+    
+    guard let wifi = currentWifi else {
+      // 와이파이 연결이 아니거나(셀룰러, 이더넷), 네트워크 연결이 없는 경우.
+      let defaultWifi = Wifi(updatedDate: Date(), ssid: "None", bssid: "None", rssi: 0.0)
+      return defaultWifi
+    }
+    
+    let updatedDate = Date()
+    let ssid = wifi.ssid
+    let bssid = wifi.bssid
+    let rssi = wifi.signalStrength
+    
+    return Wifi(updatedDate: updatedDate, ssid: ssid, bssid: bssid, rssi: rssi)
+  }
+}
