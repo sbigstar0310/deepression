b49e5da7a050f9def33ffb61ea8b457a2d9e4512
sbigstar0310 <sbigstar0930@gmail.com>
2024-06-28 16:39:41 +0900

    #update : Motion의 데이터 타입에 따라, 파일 경로를 지정하여 서버에 업데이트 하도록 변경

---
+++ deepression/Manager/FBRealtimeDataManager.swift
@@ -0,0 +1,103 @@
+//
+//  FBRealtimeDatabaseManager.swift
+//  deepression
+//
+//  Created by 성대규 on 6/18/24.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseAuth
+
+class FBRealtimeDataManager: ObservableObject {
+  @Published var userData: User?
+  let ref: DatabaseReference?
+  private let encoder: JSONEncoder
+  private let decoder: JSONDecoder
+  
+  init() {
+    self.ref = Database.database().reference()
+    self.encoder = JSONEncoder()
+    self.decoder = JSONDecoder()
+  }
+  
+  enum FBRealtimeDataManagerError: Error {
+    case referenceUnavailable
+  }
+  
+  func addLocationData(user: User, location: Location) async throws {
+    // 유저 정보 (User Info)
+    guard let ref = ref else {
+      print("error in getting ref")
+      throw FBRealtimeDataManagerError.referenceUnavailable
+    }
+    
+    let childRef = ref.child("Users").child("\(user.id)").child("Locations").childByAutoId()
+    
+    do {
+      try await childRef.setValue([
+        "id" : childRef.key,
+        "latitude" : "\(location.latitude)",
+        "longitude" : "\(location.longitude)",
+        "obtained_at" : fbDateFormatter.string(from: location.updatedDate),
+      ])
+    } catch {
+      throw error
+    }
+  }
+  
+  func addWifiData(user: User, wifi: Wifi) async throws {
+    guard let ref = ref else {
+      print("error in getting ref")
+      throw FBRealtimeDataManagerError.referenceUnavailable
+    }
+    
+    let childRef = ref.child("Users").child("\(user.id)").child("Wifies").childByAutoId()
+    
+    do {
+      try await childRef.setValue([
+        "id" : childRef.key,
+        "ssid" : "\(wifi.ssid)",
+        "bssid" : "\(wifi.bssid)",
+        "rssi" : "\(wifi.rssi)",
+        "obtained_at" : fbDateFormatter.string(from: wifi.updatedDate),
+      ])
+    } catch {
+      throw error
+    }
+  }
+  
+  func addMotionData(user: User, motion: Motion) async throws {
+    guard let ref = ref else {
+      print("error in getting ref")
+      throw FBRealtimeDataManagerError.referenceUnavailable
+    }
+    
+    var filePath: String {
+      switch motion.dataType {
+      case .accelerationField:
+        return "Acceleration"
+      case .mangeticField:
+        return "Magnetic"
+      }
+    }
+    
+    let childRef = ref.child("Users").child("\(user.id)").child(filePath).childByAutoId()
+    
+    do {
+      try await childRef.setValue([
+        "id" : childRef.key,
+        "x" : motion.x,
+        "y" : motion.y,
+        "z" : motion.z,
+        "obtained_at" : fbDateFormatter.string(from: motion.updatedDate),
+      ])
+    } catch {
+      throw error
+    }
+  }
+  
+  func deleteUserData(user: User) {
+    ref?.child("Users/\(user.id)").removeValue()
+  }
+}
